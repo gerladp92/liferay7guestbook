d804337def702447c59764958f1983d4f047481b
bartokt <tamas.bartok-nagy@p92.hu>
2018-03-21 15:11:21 +0100

    Writing an Administrative Portlet (working, yeah)

--- modules/guestbook-web/src/main/java/com/liferay/docs/guestbook/constants/GuestbookPortletKeys.java
+++ modules/guestbook-web/src/main/java/com/liferay/docs/guestbook/constants/GuestbookPortletKeys.java
@@ -4,7 +4,6 @@ package com.liferay.docs.guestbook.constants;
  * @author gregory
  */
 public class GuestbookPortletKeys {
-
 	public static final String GUESTBOOK = "com_liferay_docs_guestbook_portlet_GuestbookPortlet";
-
+	public static final String GUESTBOOK_ADMIN = "com_liferay_docs_guestbook_portlet_GuestbookAdminPortlet";
 }
